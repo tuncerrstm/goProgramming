56901ebc304dcfe7ee982437f5ce566ee1207012
tuncerrstm <tuncerrstm@gmail.com>
2021-12-07 23:23:37 +0100

    firts-program
    GO-testing

---
+++ first-program/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"conditionals"
+)
+
+func main() {
+
+	conditionals.Demo1()
+}
---
+++ first-program/workshop1.go
@@ -0,0 +1,20 @@
+package conditionals
+
+import "fmt"
+
+func Demo1() {
+
+	var sayi1, sayi2, sayi3 int = 10, 5, 18
+
+	var enBuyuk int = sayi1
+
+	if sayi2 > enBuyuk {
+		enBuyuk = sayi2
+	}
+
+	if sayi3 > enBuyuk {
+		enBuyuk = sayi3
+	}
+
+	fmt.Printf("En büyük sayı : %v", enBuyuk)
+}
